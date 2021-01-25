72a67697c45f0ed50bf920ba76be99bf08a6cf4d
fred-woolf-vk <77647466+fred-woolf-vk@users.noreply.github.com>
2021-01-25 10:21:23 -0500

    Add files via upload
    Update install script for prometheus config changes

--- puck_monitoring_install_script.sh
+++ puck_monitoring_install_script.sh
@@ -32,27 +32,26 @@ wget https://raw.githubusercontent.com/fred-woolf-vk/puck_monitoring/master/puck
 wget https://raw.githubusercontent.com/fred-woolf-vk/puck_monitoring/master/puck_monitoring_service.service
 wget https://raw.githubusercontent.com/fred-woolf-vk/puck_monitoring/master/prometheus
 echo "--------------------------------------------------------------------------------"
+
 sudo chmod 755 collect_db_stats.py
 sudo chmod 755 get_modem_status.py
 sudo chmod 777 puck_monitoring_script.sh
-sudo chmod 755 prometheus
+sudo chmod 755 prometheus 
+sudo mv puck_monitoring_service.service /etc/systemd/system
 
 echo "--------------------------------------------------------------------------------"
-systemctl stop puck_monitoring_service
-systemctl stop prometheus
 
-# prometheus service starts automatically; set prometheus max storage for 2 days
-#   in /etc/default/prometheus and restart below
-sudo mv -f prometheus /etc/default
-sudo mv -f puck_monitoring_service.service /etc/systemd/system
 sudo systemctl daemon-reload
-
 # start puck monitoring service
-systemctl enable puck_monitoring_service
-systemctl start puck_monitoring_service
+systemctl stop puck_monitoring_service.service
+systemctl enable puck_monitoring_service.service
+systemctl start puck_monitoring_service.service
 
-# start prometheus service
-systemctl enable prometheus
-systemctl start prometheus
-echo "--------------------------------------------------------------------------------"
+
+# prometheus service starts automatically; set prometheus max storage 
+sudo mv prometheus /etc/default/
+sudo stop prometheus
+sudo systemctl enable prometheus
+sudo systemctl start prometheus
+
 
