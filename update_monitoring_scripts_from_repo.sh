69199df6f955186935efaea0fd53d715b0bcf227
fred-woolf-vk <77647466+fred-woolf-vk@users.noreply.github.com>
2021-06-02 09:44:41 -0400

    Add files via upload

--- update_monitoring_scripts_from_repo.sh
+++ update_monitoring_scripts_from_repo.sh
@@ -8,8 +8,8 @@ rm -f collect_db_stats_prometheus.py
 rm -f get_modem_status.py
 
 echo " ----------------------   get updated files "
-wget https://raw.githubusercontent.com/fred-woolf-vk/gw6200_scripts/master/collect_db_stats_prometheus.py
-wget https://raw.githubusercontent.com/fred-woolf-vk/gw6200_scripts/master/get_modem_status.py
+wget https://raw.githubusercontent.com/fred-woolf-vk/puck_monitoring/master/collect_db_stats_prometheus.py
+wget https://raw.githubusercontent.com/fred-woolf-vk/puck_monitoring/master/get_modem_status.py
 
 chmod 755 collect_db_stats_prometheus.py
 chmod 755 get_modem_status.py
